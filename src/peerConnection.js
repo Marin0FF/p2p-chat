22a5c631b2eaab7ef55c72583b0b1e90af5a8147
Marin0FF <hmarinov1977@gmail.com>
2022-02-12 14:11:54 +0000

    peer conection & stun servers set up

---
+++ src/peerConnection.js
@@ -0,0 +1,13 @@
+// STUN SERVER URL'S
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+
+const pc = new RTCPeerConnection(servers);
+
+export default pc;
